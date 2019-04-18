8645d917471b967938f39268dd542986b3248cdf
Shalom Crown <shalomcrown@gmail.com>
2019-04-18 15:00:05 +0300

    Separate event and video threads

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,30 @@ const char *argp_program_bug_address = "<shalomcrown@gmail.com>";
 
 bool realTime = false;
 char *source = nullptr;
+const char *fileName = source;
+volatile bool keepWorking = true;
 
+AVFormatContext *pFormatCtx = nullptr;
+AVCodec *pVideoCodec = nullptr;
+AVStream *pVideoStream = nullptr;
+int videoStream = -1;
+int klvStream = -1;
+AVCodecParameters *pvideoCodecparameters = nullptr;
+AVCodecParameters *pklvCodecparameters = nullptr;
+uint8_t *dst_data[4];
+int src_linesize[4], dst_linesize[4];
+SDL_Window *win;
+double lastFrameTimeSeconds = 0;
+time_t lastFrameWallclockTime = time(nullptr);
+AVPacket *pPacket;
+AVCodecContext *pCodecContext;
+AVFrame *pFrame;
+int response = 0;
+
+int window_width;
+int window_height;
+SDL_Renderer *ren;
+SDL_Texture *texture;
 
 // =====================================================
 
@@ -81,140 +104,10 @@ void hexData(uint8_t *data, uint8_t *output, int length)  {
     output[x] = 0;
 }
 
-// ========================================================
 
-int main ( int argc, char *argv[] ) {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_EVENTS);
-    av_register_all();
-    
-    struct argp args = {options, parse_opt, nullptr, nullptr, nullptr, nullptr, nullptr};
-    argp_parse(&args, argc, argv, 0, 0, 0);
-    
-    const char *fileName = source;
+int sdlVideoThread(void *) {
 
-    AVFormatContext *pFormatCtx = nullptr;
-    AVCodec *pVideoCodec = nullptr;
-    AVStream *pVideoStream = nullptr;
-    int videoStream = -1;
-    int klvStream = -1;
-    AVCodecParameters *pvideoCodecparameters = nullptr;
-    AVCodecParameters *pklvCodecparameters = nullptr;
-    uint8_t *dst_data[4];
-    int src_linesize[4], dst_linesize[4];
-    SDL_Window *win;
-    double lastFrameTimeSeconds = 0;
-    time_t lastFrameWallclockTime = time(nullptr);
-
-    if (avformat_open_input(&pFormatCtx, fileName, nullptr, nullptr) != 0) {
-
-        fprintf(stderr, "Couldn't open file\n");
-        return -1;
-    }
-
-    if (avformat_find_stream_info(pFormatCtx, nullptr) < 0) {
-        fprintf(stderr, "Couldn't find stream information\n");
-        return -1;
-    }
-
-    av_dump_format(pFormatCtx, 0, fileName, 0);
-
-    for(int iStream = 0; iStream < pFormatCtx->nb_streams; iStream++) {
-        AVCodec *pLocalCodec = nullptr;
-        AVCodecParameters *pLocalCodecParameters = pFormatCtx->streams[iStream]->codecpar;
-        pLocalCodec = avcodec_find_decoder(pLocalCodecParameters->codec_id);
-        AVStream *stream = pFormatCtx->streams[iStream];
-
-        if (pLocalCodecParameters->codec_type == AVMEDIA_TYPE_DATA) {
-            klvStream = iStream;
-            pklvCodecparameters = pLocalCodecParameters;
-            printf("\tKLV stream found at %d\n", iStream);
-        }
-
-        if (pLocalCodec == nullptr) {
-            fprintf(stderr, "No codec for stream %d\n", iStream);
-            continue;
-        }
-
-        if (pLocalCodecParameters->codec_type == AVMEDIA_TYPE_VIDEO) {
-            videoStream = iStream;
-            pVideoCodec = pLocalCodec;
-            pvideoCodecparameters = pLocalCodecParameters;
-            pVideoStream = stream;
-            printf("\tCodec for stream %d: %s ID %d bit_rate %ld\n", iStream, pLocalCodec->name, pLocalCodec->id, pLocalCodecParameters->bit_rate);
-        }
-    }
-
-    if (videoStream == -1) {
-        fprintf(stderr, "Couldn't find a video stream\n");
-        return -1;
-    }
-
-    AVCodecContext *pCodecContext = avcodec_alloc_context3(pVideoCodec);
-
-    if (!pCodecContext) {
-        fprintf(stderr, "failed to allocated memory for AVCodecContext\n");
-        return -1;
-    }
-
-    if (avcodec_parameters_to_context(pCodecContext, pvideoCodecparameters) < 0) {
-        fprintf(stderr, "failed to copy codec params to codec context\n");
-        return -1;
-    }
-
-    if (avcodec_open2(pCodecContext, pVideoCodec, NULL) < 0) {
-        fprintf(stderr, "failed to open codec through avcodec_open2\n");
-        return -1;
-    }
-
-
-    AVFrame *pFrame = av_frame_alloc();
-    if (!pFrame) {
-        fprintf(stderr, "failed to allocated memory for AVFrame\n");
-        return -1;
-    }
-
-    AVPacket *pPacket = av_packet_alloc();
-    if (!pPacket) {
-        fprintf(stderr, "failed to allocated memory for AVPacket\n");
-        return -1;
-    }
-
-    int response = 0;
-
-
-    int window_width = pvideoCodecparameters->width;
-    int window_height = pvideoCodecparameters->height;
-
-    win = SDL_CreateWindow(fileName, 100, 100, window_width, window_height, SDL_WINDOW_SHOWN);
-
-    if (win == nullptr) {
-        fprintf(stderr, "Coudln't open window\n");
-        SDL_Quit();
-        return -1;
-    }
-
-    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-    if (ren == nullptr){
-        SDL_DestroyWindow(win);
-        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-        return -1;
-    }
-
-    SDL_Texture *texture = SDL_CreateTexture(ren, SDL_PIXELFORMAT_IYUV,
-                                             SDL_TEXTUREACCESS_STREAMING,
-                                             window_width, window_height);
-
-    if (! texture) {
-        fprintf(stderr, "Failed to create texture\n");
-        return -1;
-    }
-
-    SDL_SetRenderDrawColor(ren, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderPresent(ren);
-    SDL_RenderClear(ren);
-
-    while (av_read_frame(pFormatCtx, pPacket) >= 0) {
+    while (av_read_frame(pFormatCtx, pPacket) >= 0 && keepWorking) {
 
         // if it's the video stream
         if (pPacket->stream_index == videoStream) {
@@ -327,6 +220,147 @@ int main ( int argc, char *argv[] ) {
     av_packet_free(&pPacket);
     av_frame_free(&pFrame);
     avcodec_free_context(&pCodecContext);
+    SDL_Quit();
+}
+
+
+// ========================================================
+
+int main ( int argc, char *argv[] ) {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_EVENTS);
+    av_register_all();
+    
+    struct argp args = {options, parse_opt, nullptr, nullptr, nullptr, nullptr, nullptr};
+    argp_parse(&args, argc, argv, 0, 0, 0);
+    
+    fileName = source;
+
+    if (avformat_open_input(&pFormatCtx, fileName, nullptr, nullptr) != 0) {
+
+        fprintf(stderr, "Couldn't open file\n");
+        return -1;
+    }
+
+    if (avformat_find_stream_info(pFormatCtx, nullptr) < 0) {
+        fprintf(stderr, "Couldn't find stream information\n");
+        return -1;
+    }
+
+    av_dump_format(pFormatCtx, 0, fileName, 0);
+
+    for(int iStream = 0; iStream < pFormatCtx->nb_streams; iStream++) {
+        AVCodec *pLocalCodec = nullptr;
+        AVCodecParameters *pLocalCodecParameters = pFormatCtx->streams[iStream]->codecpar;
+        pLocalCodec = avcodec_find_decoder(pLocalCodecParameters->codec_id);
+        AVStream *stream = pFormatCtx->streams[iStream];
+
+        if (pLocalCodecParameters->codec_type == AVMEDIA_TYPE_DATA) {
+            klvStream = iStream;
+            pklvCodecparameters = pLocalCodecParameters;
+            printf("\tKLV stream found at %d\n", iStream);
+        }
+
+        if (pLocalCodec == nullptr) {
+            fprintf(stderr, "No codec for stream %d\n", iStream);
+            continue;
+        }
+
+        if (pLocalCodecParameters->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStream = iStream;
+            pVideoCodec = pLocalCodec;
+            pvideoCodecparameters = pLocalCodecParameters;
+            pVideoStream = stream;
+            printf("\tCodec for stream %d: %s ID %d bit_rate %ld\n", iStream, pLocalCodec->name, pLocalCodec->id, pLocalCodecParameters->bit_rate);
+        }
+    }
+
+    if (videoStream == -1) {
+        fprintf(stderr, "Couldn't find a video stream\n");
+        return -1;
+    }
+
+    pCodecContext = avcodec_alloc_context3(pVideoCodec);
+
+    if (!pCodecContext) {
+        fprintf(stderr, "failed to allocated memory for AVCodecContext\n");
+        return -1;
+    }
+
+    if (avcodec_parameters_to_context(pCodecContext, pvideoCodecparameters) < 0) {
+        fprintf(stderr, "failed to copy codec params to codec context\n");
+        return -1;
+    }
+
+    if (avcodec_open2(pCodecContext, pVideoCodec, NULL) < 0) {
+        fprintf(stderr, "failed to open codec through avcodec_open2\n");
+        return -1;
+    }
+
+
+    pFrame = av_frame_alloc();
+    if (!pFrame) {
+        fprintf(stderr, "failed to allocated memory for AVFrame\n");
+        return -1;
+    }
+
+    pPacket = av_packet_alloc();
+    if (!pPacket) {
+        fprintf(stderr, "failed to allocated memory for AVPacket\n");
+        return -1;
+    }
+
+    response = 0;
+
+    window_width = pvideoCodecparameters->width;
+    window_height = pvideoCodecparameters->height;
+
+    win = SDL_CreateWindow(fileName, 100, 100, window_width, window_height, SDL_WINDOW_SHOWN);
+
+    if (win == nullptr) {
+        fprintf(stderr, "Coudln't open window\n");
+        SDL_Quit();
+        return -1;
+    }
+
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (ren == nullptr){
+        SDL_DestroyWindow(win);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    texture = SDL_CreateTexture(ren, SDL_PIXELFORMAT_IYUV,
+                                             SDL_TEXTUREACCESS_STREAMING,
+                                             window_width, window_height);
+
+    if (! texture) {
+        fprintf(stderr, "Failed to create texture\n");
+        return -1;
+    }
+
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderPresent(ren);
+    SDL_RenderClear(ren);
+
+    SDL_Thread *thread = SDL_CreateThread(sdlVideoThread, "Video thread", nullptr);
+
+    SDL_Event event;
+
+    while (SDL_QuitRequested() == false) {
+        while (SDL_PollEvent(&event)) {
+
+            if (event.type == SDL_WINDOWEVENT) {
+                switch (event.window.event) {
+                    case SDL_WINDOWEVENT_CLOSE:
+                        keepWorking = false;
+                        SDL_Quit();
+                        break;
+                }
+            }
+        }
+    }
+
     SDL_Quit();
     return 0;
 }
